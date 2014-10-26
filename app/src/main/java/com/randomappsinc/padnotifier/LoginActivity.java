30ddc231462f632de37c5f8393ac3d55225c8656
Loche <bakafish@gmail.com>
2014-10-26 04:29:21 -0700

    Now with tabs.
    The tabs don't go anywhere, but they're there.

--- app/src/main/java/com/randomappsinc/padnotifier/LoginActivity.java
+++ app/src/main/java/com/randomappsinc/padnotifier/LoginActivity.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,12 +45,13 @@ public class LoginActivity extends Activity {
     }
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     View.OnClickListener loginSubmitListener = new View.OnClickListener() {
         public void onClick(View v) {
@@ -75,15 +79,15 @@ public class LoginActivity extends Activity {
         }
     };
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/randomappsinc/padnotifier/MainActivity.java
+++ app/src/main/java/com/randomappsinc/padnotifier/MainActivity.java
@@ -1,18 +1,68 @@
 package com.randomappsinc.padnotifier;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements
+        ActionBar.TabListener {
+
+    private DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+    private ViewPager mViewPager;
+
+    public MainActivity() {
+    }
+//    private Tab metalsTab, godfestTab, settingsTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialization
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        ActionBar actionBar = getActionBar();
+        actionBar.setIcon(R.drawable.icon_177);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+//        metalsTab = actionBar.newTab().setText("Metals");
+//        godfestTab = actionBar.newTab().setText("Godfest");
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener()
+        {
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // show the given tab
+            }
+
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // hide the given tab
+            }
+
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // probably ignore this event
+            }
+        };
+
+        // Add two tabs, specifying the tab's text and TabListener
+        // Metals
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Metals")
+                        .setTabListener(tabListener));
+
+        // Godfest
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Godfest")
+                        .setTabListener(tabListener));
 
         Intent intent = getIntent();
         String group = intent.getStringExtra(LoginActivity.groupKey);
@@ -23,9 +73,10 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -38,5 +89,20 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction fragmentTransaction) {
+
     }
 }
